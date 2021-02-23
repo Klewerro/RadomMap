1615a5f2b9794a9b33d985b3eb2456d498eb12cc
PSzymon <pol.szym95@gmail.com>
2021-02-23 21:20:17 +0100

    MapsActivity renamed to MainActivity. Strings.xml for polish language

--- app/src/main/java/com/klewerro/radommap/MainActivity.kt
+++ app/src/main/java/com/klewerro/radommap/MainActivity.kt
@@ -7,13 +7,13 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 
-class MapsActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
+        setContentView(R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.navigation_fragment_container) as NavHostFragment
